0c13162cb0240860d3f3ae6b799f68b0a4e17a89
alaaDragneel <moaalaa16@gmail.com>
2017-11-19 15:11:17 +0200

    first commit

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+    <div class="row">
+        <app-quote v-for="quote in quotes">{{ quote }}</app-quote>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="css">
+</style>
